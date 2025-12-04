c32a5a91ba1bec700111836518dbaace0d0c7102
privatecore <privatecorepp@gmail.com>
2025-12-04 11:25:46 +0100

    Add ItemQuality to the game constants export

--- src/Bindings/Constants/GameEnums.cpp
+++ src/Bindings/Constants/GameEnums.cpp
@@ -10,22 +10,22 @@ void export_game_constants()
 {
     // enum Gender
     bp::enum_<Gender>("Gender")
-        .value("Male",          GENDER_MALE)        // 0
-        .value("Female",        GENDER_FEMALE)      // 1
+        .value("Male",          GENDER_MALE)    // 0
+        .value("Female",        GENDER_FEMALE)  // 1
         .export_values();
 
     // enum Races (ChrRaces.dbc)
     bp::enum_<Races>("Race")
-        .value("Human",         RACE_HUMAN)          // 1
-        .value("Orc",           RACE_ORC)            // 2
-        .value("Dwarf",         RACE_DWARF)          // 3
-        .value("Nightelf",      RACE_NIGHTELF)       // 4
-        .value("Undead",        RACE_UNDEAD_PLAYER)  // 5
-        .value("Tauren",        RACE_TAUREN)         // 6
-        .value("Gnome",         RACE_GNOME)          // 7
-        .value("Troll",         RACE_TROLL)          // 8
-        .value("Bloodelf",      RACE_BLOODELF)       // 10
-        .value("Draenei",       RACE_DRAENEI)        // 11
+        .value("Human",         RACE_HUMAN)         // 1
+        .value("Orc",           RACE_ORC)           // 2
+        .value("Dwarf",         RACE_DWARF)         // 3
+        .value("Nightelf",      RACE_NIGHTELF)      // 4
+        .value("Undead",        RACE_UNDEAD_PLAYER) // 5
+        .value("Tauren",        RACE_TAUREN)        // 6
+        .value("Gnome",         RACE_GNOME)         // 7
+        .value("Troll",         RACE_TROLL)         // 8
+        .value("Bloodelf",      RACE_BLOODELF)      // 10
+        .value("Draenei",       RACE_DRAENEI)       // 11
         .export_values();
 
     // enum Classes (ChrClasses.dbc)
@@ -44,11 +44,11 @@ void export_game_constants()
 
     // enum Stats
     bp::enum_<Stats>("Stat")
-        .value("Strength",      STAT_STRENGTH)      // 0
-        .value("Agility",       STAT_AGILITY)       // 1
-        .value("Stamina",       STAT_STAMINA)       // 2
-        .value("Intellect",     STAT_INTELLECT)     // 3
-        .value("Spirit",        STAT_SPIRIT)        // 4
+        .value("Strength",      STAT_STRENGTH)  // 0
+        .value("Agility",       STAT_AGILITY)   // 1
+        .value("Stamina",       STAT_STAMINA)   // 2
+        .value("Intellect",     STAT_INTELLECT) // 3
+        .value("Spirit",        STAT_SPIRIT)    // 4
         .export_values();
 
     // enum Powers
@@ -60,5 +60,17 @@ void export_game_constants()
         .value("Happiness",     POWER_HAPPINESS)    // 4
         .value("Rune",          POWER_RUNE)         // 5
         .value("RunicPower",    POWER_RUNIC_POWER)  // 6
+        .export_values();
+
+    // enum ItemQualities
+    bp::enum_<ItemQualities>("ItemQuality")
+        .value("Poor",          ITEM_QUALITY_POOR)      // 0
+        .value("Normal",        ITEM_QUALITY_NORMAL)    // 1
+        .value("Uncommon",      ITEM_QUALITY_UNCOMMON)  // 2
+        .value("Rare",          ITEM_QUALITY_RARE)      // 3
+        .value("Epic",          ITEM_QUALITY_EPIC)      // 4
+        .value("Legendary",     ITEM_QUALITY_LEGENDARY) // 5
+        .value("Artifact",      ITEM_QUALITY_ARTIFACT)  // 6
+        .value("Heirloom",      ITEM_QUALITY_HEIRLOOM)  // 7
         .export_values();
 }
